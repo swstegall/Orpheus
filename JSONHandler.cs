76facf4a00e72a9eaf8064ea692c9523d7362bc3
Isaac <isaactij@sbcglobal.net>
2020-10-05 21:15:47 -0500

    Feature 2
    Created JSON schema and implemented changes to allow the application to read the JSON file on startup

---
+++ JSONHandler.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orpheus {
+    class JSONHandler {
+
+        //Will read the music_storage json file and return a SongList object of all the songs in the file - Isaac
+        public SongList ReadJsonFile() {
+            //Object to be returned - Isaac
+            SongList MusicFromJSON;
+            try {
+                string JSONFromFile;
+                //Builds the path based on the location of the application in the current user's directory  - Isaac
+                //If the path is incorrect in testing on your machine let me know and I will try someting different - Isaac
+                string path = Path.Combine(Environment.CurrentDirectory, @"music_storage.json");
+                using (var reader = new StreamReader(path)) {
+                    JSONFromFile = reader.ReadToEnd();
+                }
+                //Deserializes the text from the file using Newtonsoft's tool - Isaac
+                MusicFromJSON = JsonConvert.DeserializeObject<SongList>(JSONFromFile);
+            }
+            catch (Exception ex){
+                //Initializes the object to be returned to a empty variable if the file could not be found - Isaac
+                MusicFromJSON = new SongList();
+            }
+            return MusicFromJSON;
+        }
+
+    }
+}
---
+++ SongList.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orpheus {
+    class SongList {
+        //All variables in this class need a JsonProperty attribute - Isaac
+        //This is because the variables here are present and ordered exactly like the objects in music_storeage.json which is necessary for deserialization - Isaac
+
+        //This is a list of song objects - Isaac
+        //This is the root level of music_storage.json - Isaac
+        [JsonProperty("Songs")]
+        public List<SongLocation> List {get; set;}
+
+    }
+}
