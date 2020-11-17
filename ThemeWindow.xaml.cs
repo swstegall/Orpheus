7d960454a8dfcdccafdfdb1ad51a40dc52df2ed0
Sam Stegall <stegall@medicine.tamhsc.edu>
2020-11-17 16:18:16 -0600

    Setup improved theme window.

---
+++ ThemeWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Orpheus
+{
+    /// <summary>
+    /// Interaction logic for ThemeWindow.xaml
+    /// </summary>
+    public partial class ThemeWindow : Window
+    {
+        public ThemeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CbTest_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
