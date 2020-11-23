5283c4e01c432ccb78492e44db5936f9ecac9a52
Samuel Wallace Stegall <stegall@tamu.edu>
2020-11-22 18:26:16 -0600

    Setup ThemeSelector class, as well as added guards for escaping popup.

---
+++ Utilities/ThemeSelector.cs
@@ -0,0 +1,148 @@
+﻿namespace Orpheus.Utilities
+{
+    public class ThemeSelector
+    {
+        public (string, string, string) SelectTheme(string ThemeName)
+        {
+            (string, string, string) vals = ("", "", "");
+            switch (ThemeName)
+            {
+                case "Default (Light)":
+                    {
+                        vals.Item1 = "#000000";
+                        vals.Item2 = "#FFFFFF";
+                        vals.Item3 = "#FF0000";
+                        break;
+                    }
+                case "Luna":
+                    {
+                        vals.Item1 = "#00FFFF";
+                        vals.Item2 = "#0000FF";
+                        vals.Item3 = "#00FF00";
+                        break;
+                    }
+                case "S'mores":
+                    {
+                        vals.Item1 = "#D2B48C";
+                        vals.Item2 = "#A52A2A";
+                        vals.Item3 = "#FFFFFF";
+                        break;
+                    }
+                case "Candyland":
+                    {
+                        vals.Item1 = "#ADD8E6";
+                        vals.Item2 = "#FFC0CB";
+                        vals.Item3 = "#EE82EE";
+                        break;
+                    }
+                case "Halloween":
+                    {
+                        vals.Item1 = "#FFA500";
+                        vals.Item2 = "#000000";
+                        vals.Item3 = "#FFFF00";
+                        break;
+                    }
+                case "Thanksgiving":
+                    {
+                        vals.Item1 = "#FFA500";
+                        vals.Item2 = "#A52A2A";
+                        vals.Item3 = "#FFFF00";
+                        break;
+                    }
+                case "Christmas":
+                    {
+                        vals.Item1 = "#FFFF00";
+                        vals.Item2 = "#00FF00";
+                        vals.Item3 = "#FF0000";
+                        break;
+                    }
+                case "Reveille (Light)":
+                    {
+                        vals.Item1 = "#500000";
+                        vals.Item2 = "#FFFFFF";
+                        vals.Item3 = "#500000";
+                        break;
+                    }
+                case "Reveille (Dark)":
+                    {
+                        vals.Item1 = "#500000";
+                        vals.Item2 = "#999999";
+                        vals.Item3 = "#500000";
+                        break;
+                    }
+                case "Hunter (Light)":
+                    {
+                        vals.Item1 = "#018744";
+                        vals.Item2 = "#FFFFFF";
+                        vals.Item3 = "#C24444";
+                        break;
+                    }
+                case "Hunter (Dark)":
+                    {
+                        vals.Item1 = "#018744";
+                        vals.Item2 = "#999999";
+                        vals.Item3 = "#C24444";
+                        break;
+                    }
+                case "Classic":
+                    {
+                        vals.Item1 = "#234508";
+                        vals.Item2 = "#DDA0DD";
+                        vals.Item3 = "#C67676";
+                        break;
+                    }
+                case "Rose":
+                    {
+                        vals.Item1 = "#234555";
+                        vals.Item2 = "#C21E56";
+                        vals.Item3 = "#C67777";
+                        break;
+                    }
+                case "Classic Gold":
+                    {
+                        vals.Item1 = "#234656";
+                        vals.Item2 = "#8F7034";
+                        vals.Item3 = "#C67878";
+                        break;
+                    }
+                case "Classic Teal":
+                    {
+                        vals.Item1 = "#234444";
+                        vals.Item2 = "#038387";
+                        vals.Item3 = "#C67171";
+                        break;
+                    }
+                case "Classic Plum":
+                    {
+                        vals.Item1 = "#234848";
+                        vals.Item2 = "#854085";
+                        vals.Item3 = "#C67879";
+                        break;
+                    }
+                case "Cool Grey":
+                    {
+                        vals.Item1 = "#233147";
+                        vals.Item2 = "#737373";
+                        vals.Item3 = "#C67870";
+                        break;
+                    }
+                case "Warm Grey":
+                    {
+                        vals.Item1 = "#234348";
+                        vals.Item2 = "#867365";
+                        vals.Item3 = "#C57278";
+                        break;
+                    }
+                case "Default (Dark)":
+                default:
+                    {
+                        vals.Item1 = "#FFFFFF";
+                        vals.Item2 = "#999999";
+                        vals.Item3 = "#FF0000";
+                        break;
+                    }
+            }
+            return vals;
+        }
+    }
+}
