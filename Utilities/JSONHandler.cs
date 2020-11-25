538aa3c9f46dade1fc7609afc1c97b4f4bf4163f
Isaac <isaactij@sbcglobal.net>
2020-11-24 20:49:57 -0600

    Added comments

--- Utilities/JSONHandler.cs
+++ Utilities/JSONHandler.cs
@@ -12,6 +12,7 @@ namespace Orpheus.Utilities
         //Will read the music_storage json file and return a SongList object of all the songs in the file - Isaac
         public SongList ReadJsonFile()
         {
+            //Object that is read from the JSON file - Isaac
             JSONData DataFromJSON;
             //Object to be returned - Isaac
             SongList MusicFromJSON = new SongList();
@@ -27,6 +28,7 @@ namespace Orpheus.Utilities
                 }
                 //Deserializes the text from the file using Newtonsoft's tool - Isaac
                 DataFromJSON = JsonConvert.DeserializeObject<JSONData>(JSONFromFile);
+                //Separate them from SongList data
                 theme = DataFromJSON.Theme;
                 MusicFromJSON.List = DataFromJSON.SongList;
             }
@@ -48,10 +50,11 @@ namespace Orpheus.Utilities
                 //Builds the path based on the location of the application in the current user's directory  - Isaac
                 //If the path is incorrect in testing on your machine let me know and I will try someting different - Isaac
                 string path = Path.Combine(Environment.CurrentDirectory, @"music_storage.json");
+                //Constructs the JSON data to be written - Isaac
                 DataForJSON = new JSONData();
                 DataForJSON.Theme = theme;
                 DataForJSON.SongList = ListToWrite.List;
-                //Serializes the SongList object to a string with the data in JSON format - Isaac
+                //Serializes the DataForJSON object to a string with the data in JSON format - Isaac
                 string ConvertedString = JsonConvert.SerializeObject(DataForJSON);
                 using (var writer = new StreamWriter(path))
                 {
